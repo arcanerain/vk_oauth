01e0d757e594f7dbe9cf3265b054fd239ff5c0e1
Rustam Gasanov <animeame@gmail.com>
2012-01-20 02:20:42 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VkOauth::Application.config.secret_token = 'b403250220ff3ffca766729005757e6997780f47a2322f2252d587d4412d947d9355cba39ec6f3e9349f604d9b82044501011cba771fbda8ce304d4a335c3140'
